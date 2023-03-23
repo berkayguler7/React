4fa9454e5946c886259b79c69402e661fc31edc7
BerkiManman <berkayguler7@hotmail.com>
2023-03-23 17:54:10 +0300

    Props tutorial finished

--- hello-react/src/components/User.js
+++ hello-react/src/components/User.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function User({name, isLoggedIn, age, friends}) {
+    if(!isLoggedIn) return <div>Not logged in</div>;
   return (
     <div>
-        {isLoggedIn ? `User: ${name} (${age})` : 'Not logged in'}
+        {`User: ${name} (${age})`}
 
         {friends && friends.map((friend) => 
         <div key={friend.id}>{friend.name} {friend.age &&`(${friend.age})`}</div>)}
@@ -22,4 +23,9 @@ User.propTypes = {
         name: PropTypes.string.isRequired,
         age: PropTypes.number,
     })),
+}
+
+User.defaultProps = {
+    name: 'Guest',
+    isLoggedIn: false,
 }
